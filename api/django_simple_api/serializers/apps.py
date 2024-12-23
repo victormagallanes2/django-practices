94dab8264bc5602d0f8b0a064ec13f358eed2c8c
unknown <victormagallanes2@gmail.com>
2024-12-23 17:26:05 -0400

    update

--- api/django_simple_api/serializers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SerializersConfig(AppConfig):
-    name = 'serializers'
--- api/django_simple_api/swagger/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Swagger
-
-
-urlpatterns = [
-               path('swagger-custom/', Swagger.as_view()),
-              ]
